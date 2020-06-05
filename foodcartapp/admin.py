21da0df2adfb9b0ad6bfef0404fd3c1b0c3de01c
Evgeny Evseev <pelid80@gmail.com>
2020-06-05 18:54:18 +0300

    enable admin UI

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import City, Location, CustomUser, Hotel, Product, Order, OrderDetails
+
+admin.site.register(City)
+admin.site.register(Location)
+admin.site.register(CustomUser)
+admin.site.register(Hotel)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderDetails)
