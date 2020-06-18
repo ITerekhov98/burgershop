bed0abd3b45001ec29d8d9b24402493347c0e386
voron434 <070809010@mail.ru>
2020-06-18 21:57:29 +0300

    Add __str__ methods

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -8,11 +8,17 @@ class City(models.Model):
     name = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Location(models.Model):
     name = models.CharField(max_length=50)
     pincode = models.CharField(max_length=7)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class CustomUser(models.Model):
@@ -21,11 +27,17 @@ class CustomUser(models.Model):
     pincode = models.CharField(max_length=7)
     address = models.CharField(max_length=256)
 
+    def __str__(self):
+        return f"{self.user.username} {self.user.get_full_name()}"
+
 
 @receiver(post_save, sender=User, dispatch_uid='create_user_profile')
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         CustomUser.objects.create(user=instance)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Hotel(models.Model):
@@ -33,6 +45,9 @@ class Hotel(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     gst = models.DecimalField(max_digits=4, decimal_places=2)
     hoteladmin = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Product(models.Model):
@@ -45,6 +60,9 @@ class Product(models.Model):
     category = models.CharField(max_length=50)
     hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Order(models.Model):
     customer = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
@@ -54,8 +72,14 @@ class Order(models.Model):
     amount = models.DecimalField(max_digits=15, decimal_places=2)
     order_type = models.SmallIntegerField(default=1)
 
+    def __str__(self):
+        return f"{self.customer} status:{self.status} order time:{self.order_time}"
+
 
 class OrderDetails(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.DecimalField(max_digits=8, decimal_places=2)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.product} x{self.quantity} for {self.order.customer}"
