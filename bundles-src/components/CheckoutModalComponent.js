caff29f821aa81c8ea664991fc068899e028bdd7
Evgeny Evseev <pelid80@gmail.com>
2020-07-02 19:38:59 +0300

    fix bugs in CheckoutModalComponent

--- bundles-src/components/CheckoutModalComponent.js
+++ bundles-src/components/CheckoutModalComponent.js
@@ -4,10 +4,11 @@ import {Button} from 'react-bootstrap';
 
 class CheckoutModal extends Component{
   state = {
-    firstname:"",
-    lastname:"",
-    phonenumber:"",
-    address:""
+    firstname: "",
+    lastname: "",
+    phonenumber: "",
+    address: "",
+    waitTillCheckoutEnds: false,
   }
 
   saveFirstname = event => {
@@ -38,43 +39,53 @@ class CheckoutModal extends Component{
     });
   }
 
-  submit = e => {
-    e.preventDefault();
-    this.props.handleCheckout(this.state);
+  async submit(event){
+    event.preventDefault();
+
+    this.setState({
+      waitTillCheckoutEnds : true,
+    });
+
+    try {
+      await this.props.handleCheckout(this.state);
+
+    } finally {
+      this.setState({
+        waitTillCheckoutEnds : false,
+      });
+    }
   }
 
   render(){
     return (
       <Modal show={this.props.checkoutModalActive} onHide={this.props.handleCheckoutModalClose}>
-        <Modal.Header closeButton>
-          <h2>
-            <center>
-              <Modal.Title>Оформление заказа</Modal.Title>
-            </center>
-          </h2>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="form-group container-fluid">
-            <label htmlFor="firstname">Имя:</label>
-            <input onChange={this.saveFirstname} required id="firstname" type="text" className="form-control"/><br/>
-            <label htmlFor="lastname">Фамилия:</label>
-            <input onChange={this.saveLastname} required id="lastname" type="text" className="form-control"/><br/>
-            <label htmlFor="phonenumber">Телефон:</label>
-            <input onChange={this.savePhonenumber} required id="phonenumber" maxLength="10" type="text" className="form-control" placeholder="+7 901 ..."/><br/>
-            <label htmlFor="address">Адрес доставки:</label>
-            <input onChange={this.saveAddress} required id="address" type="text" maxLength="256" className="form-control" placeholder="Город, улица, дом"/><br/>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-           <Button id="order-submit-btn" onClick={(events)=>{
-               document.getElementById("order-submit-btn").setAttribute("disabled","disabled");
-               this.submit(events);
-               document.getElementById("order-submit-btn").removeAttribute("disabled");
-            }} className="btn btn-primary">
+        <form onSubmit={event=>this.submit(event)}>
+          <Modal.Header closeButton>
+            <h2>
+              <center>
+                <Modal.Title>Оформление заказа</Modal.Title>
+              </center>
+            </h2>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="form-group container-fluid">
+              <label htmlFor="firstname">Имя:</label>
+              <input onChange={this.saveFirstname} required id="firstname" type="text" className="form-control"/><br/>
+              <label htmlFor="lastname">Фамилия:</label>
+              <input onChange={this.saveLastname} required id="lastname" type="text" className="form-control"/><br/>
+              <label htmlFor="phonenumber">Телефон:</label>
+              <input onChange={this.savePhonenumber} required id="phonenumber" maxLength="10" type="text" className="form-control" placeholder="+7 901 ..."/><br/>
+              <label htmlFor="address">Адрес доставки:</label>
+              <input onChange={this.saveAddress} required id="address" type="text" maxLength="256" className="form-control" placeholder="Город, улица, дом"/><br/>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button id="order-submit-btn" className="btn btn-primary" type="submit" disabled={ this.state.waitTillCheckoutEnds }>
               Отправить
             </Button>
-          <Button onClick={this.props.handleCheckoutModalClose}>Закрыть</Button>
-        </Modal.Footer>
+            <Button onClick={this.props.handleCheckoutModalClose}>Закрыть</Button>
+          </Modal.Footer>
+        </form>
       </Modal>
     );
   }
