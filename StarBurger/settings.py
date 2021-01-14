ab00c32799aa22da1087e03b6c381b43973600ee
voron434 <070809010@mail.ru>
2021-01-14 22:06:30 +0300

    Made settings tunable

--- StarBurger/settings.py
+++ StarBurger/settings.py
@@ -2,16 +2,18 @@ import os
 
 import dj_database_url
 
+from environs import Env
+
+
+env = Env()
+env.read_env()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j#jww5g6d96bi#kbfpq%mgblr8^yg8$zmmg+c6dm+bxfa*&c+l'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env('SECRET_KEY') or 'etirgvonenrfnoerngorenogneongg334g'
+DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
