5aefec9cb292450c1ae7f88cc18be114c6f18998
voron434 <070809010@mail.ru>
2021-01-15 17:18:52 +0300

    Made ALLOWED_HOSTS setting tunable

--- StarBurger/settings.py
+++ StarBurger/settings.py
@@ -15,7 +15,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = env('SECRET_KEY', 'etirgvonenrfnoerngorenogneongg334g')
 DEBUG = env.bool('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
