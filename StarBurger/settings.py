9405b92406cd863f3e4ee86790ec889fc5bac4ee
voron434 <070809010@mail.ru>
2021-01-15 16:31:50 +0300

    Fix defaults for settings

--- StarBurger/settings.py
+++ StarBurger/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-SECRET_KEY = env('SECRET_KEY') or 'etirgvonenrfnoerngorenogneongg334g'
+SECRET_KEY = env('SECRET_KEY', 'etirgvonenrfnoerngorenogneongg334g')
 DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = []
