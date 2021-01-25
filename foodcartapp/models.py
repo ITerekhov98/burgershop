9989dbd4afbf98103ba63219f33896181af1dfe9
voron434 <070809010@mail.ru>
2021-01-25 18:36:54 +0300

    Restrict price lower than 0

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Restaurant(models.Model):
@@ -34,7 +35,7 @@ class Product(models.Model):
     name = models.CharField('название', max_length=50)
     category = models.ForeignKey(ProductCategory, null=True, blank=True, on_delete=models.SET_NULL,
                                  verbose_name='категория', related_name='products')
-    price = models.DecimalField('цена', max_digits=8, decimal_places=2)
+    price = models.DecimalField('цена', max_digits=8, decimal_places=2, validators=[MinValueValidator(0)])
     image = models.ImageField('картинка')
     special_status = models.BooleanField('спец.предложение', default=False, db_index=True)
     description = models.TextField('описание', max_length=200, blank=True)
