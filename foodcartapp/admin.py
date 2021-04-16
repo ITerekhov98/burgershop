6cc02c4f799ef9e936488fee8a6481d3b04f6fed
Evgeny Evseev <pelid80@gmail.com>
2021-04-16 20:03:01 +0300

    Fix admin image preview widget

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -90,14 +90,14 @@ class ProductAdmin(admin.ModelAdmin):
     def get_image_preview(self, obj):
         if not obj.image:
             return 'выберите картинку'
-        return format_html('<img src="{url}" height="200"/>', url=obj.image.url)
+        return format_html('<img src="{url}" style="max-height: 200px;"/>', url=obj.image.url)
     get_image_preview.short_description = 'превью'
 
     def get_image_list_preview(self, obj):
         if not obj.image or not obj.id:
             return 'нет картинки'
         edit_url = reverse('admin:foodcartapp_product_change', args=(obj.id,))
-        return format_html('<a href="{edit_url}"><img src="{src}" height="50"/></a>', edit_url=edit_url, src=obj.image.url)
+        return format_html('<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>', edit_url=edit_url, src=obj.image.url)
     get_image_list_preview.short_description = 'превью'
 
 
