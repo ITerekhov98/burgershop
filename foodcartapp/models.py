e8d250d63e0c8386ac6b5bf242a9ec432be91166
Evgeny Evseev <pelid80@gmail.com>
2021-04-24 08:29:25 +0300

    Format code

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -3,16 +3,27 @@ from django.core.validators import MinValueValidator
 
 
 class Restaurant(models.Model):
-    name = models.CharField('название', max_length=50)
-    address = models.CharField('адрес', max_length=100, blank=True)
-    contact_phone = models.CharField('контактный телефон', max_length=50, blank=True)
-
-    def __str__(self):
-        return self.name
+    name = models.CharField(
+        'название',
+        max_length=50
+    )
+    address = models.CharField(
+        'адрес',
+        max_length=100,
+        blank=True,
+    )
+    contact_phone = models.CharField(
+        'контактный телефон',
+        max_length=50,
+        blank=True,
+    )
 
     class Meta:
         verbose_name = 'ресторан'
         verbose_name_plural = 'рестораны'
+
+    def __str__(self):
+        return self.name
 
 
 class ProductQuerySet(models.QuerySet):
@@ -21,7 +32,10 @@ class ProductQuerySet(models.QuerySet):
 
 
 class ProductCategory(models.Model):
-    name = models.CharField('название', max_length=50)
+    name = models.CharField(
+        'название',
+        max_length=50
+    )
 
     class Meta:
         verbose_name = 'категория'
@@ -32,33 +46,66 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField('название', max_length=50)
-    category = models.ForeignKey(ProductCategory, null=True, blank=True, on_delete=models.SET_NULL,
-                                 verbose_name='категория', related_name='products')
-    price = models.DecimalField('цена', max_digits=8, decimal_places=2, validators=[MinValueValidator(0)])
-    image = models.ImageField('картинка')
-    special_status = models.BooleanField('спец.предложение', default=False, db_index=True)
-    description = models.TextField('описание', max_length=200, blank=True)
+    name = models.CharField(
+        'название',
+        max_length=50
+    )
+    category = models.ForeignKey(
+        ProductCategory,
+        verbose_name='категория',
+        related_name='products',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    price = models.DecimalField(
+        'цена',
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
+    image = models.ImageField(
+        'картинка'
+    )
+    special_status = models.BooleanField(
+        'спец.предложение',
+        default=False,
+        db_index=True,
+    )
+    description = models.TextField(
+        'описание',
+        max_length=200,
+        blank=True,
+    )
 
     objects = ProductQuerySet.as_manager()
-
-    def __str__(self):
-        return self.name
 
     class Meta:
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
 
+    def __str__(self):
+        return self.name
+
 
 class RestaurantMenuItem(models.Model):
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='menu_items',
-                                   verbose_name="ресторан")
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='menu_items',
-                                verbose_name='продукт')
-    availability = models.BooleanField('в продаже', default=True, db_index=True)
-
-    def __str__(self):
-        return f"{self.restaurant.name} - {self.product.name}"
+    restaurant = models.ForeignKey(
+        Restaurant,
+        related_name='menu_items',
+        verbose_name="ресторан",
+        on_delete=models.CASCADE,
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='menu_items',
+        verbose_name='продукт',
+    )
+    availability = models.BooleanField(
+        'в продаже',
+        default=True,
+        db_index=True
+    )
 
     class Meta:
         verbose_name = 'пункт меню ресторана'
@@ -66,3 +113,6 @@ class RestaurantMenuItem(models.Model):
         unique_together = [
             ['restaurant', 'product']
         ]
+
+    def __str__(self):
+        return f"{self.restaurant.name} - {self.product.name}"
