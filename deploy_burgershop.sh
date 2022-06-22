d4ac5467788284277c6a22dc7cb864be69f74dc0
iterekhov98@gmail.com <iterekhov98@gmail.com>
2022-06-22 16:19:02 +0000

    Extend deploying script forr production server

--- deploy_burgershop.sh
+++ deploy_burgershop.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
+source .env
 git pull
-pip install -r requirements.txt
+/opt/burgershop/env/bin/pip3 install -r requirements.txt
 npm ci --dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
-python manage.py collectstatic --noinput
-python manage.py migrate
+/opt/burgershop/env/bin/python3 manage.py collectstatic --noinput
+/opt/burgershop/env/bin/python3 manage.py migrate
 systemctl restart burgershop
 systemctl reload nginx
 
+git_revision=$(git rev-parse HEAD)
+https api.rollbar.com:443/api/1/deploy X-Rollbar-Access-Token:$ROLLBAR_TOKEN environment=$ROLLBAR_ENVIRONMENT revision=$git_revision
 echo deploy was completed successfully
