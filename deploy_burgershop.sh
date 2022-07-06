a5075df26459316756970b0ab26dcc561fdf6840
iterekhov98@gmail.com <iterekhov98@gmail.com>
2022-07-06 17:41:37 +0300

    Add ssl ability to nginx

--- deploy_burgershop.sh
+++ deploy_burgershop.sh
@@ -3,8 +3,8 @@ set -e
 
 source .env
 git pull
-docker-compose -f docker-compose.prod.yml build
-docker-compose -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.prod_ssl.yml build
+docker-compose -f docker-compose.prod_ssl.yml up -d
 git_revision=$(git rev-parse HEAD)
 https api.rollbar.com:443/api/1/deploy X-Rollbar-Access-Token:$ROLLBAR_TOKEN environment=$ROLLBAR_ENVIRONMENT revision=$git_revision
 echo deploy was completed successfully
