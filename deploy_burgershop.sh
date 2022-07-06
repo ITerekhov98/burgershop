6b0bf6d784d940b4c2ced20cd446cb36af13ac2f
iterekhov98@gmail.com <iterekhov98@gmail.com>
2022-07-06 13:10:40 +0300

    Add docker-compose for prod version

--- deploy_burgershop.sh
+++ deploy_burgershop.sh
@@ -3,14 +3,8 @@ set -e
 
 source .env
 git pull
-/opt/burgershop/env/bin/pip3 install -r requirements.txt
-npm ci --dev
-./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
-/opt/burgershop/env/bin/python3 manage.py collectstatic --noinput
-/opt/burgershop/env/bin/python3 manage.py migrate --noinput
-systemctl restart burgershop
-systemctl reload nginx
-
+docker-compose -f docker-compose.prod.yml build
+docker-compose -f docker-compose.prod.yml up -d
 git_revision=$(git rev-parse HEAD)
 https api.rollbar.com:443/api/1/deploy X-Rollbar-Access-Token:$ROLLBAR_TOKEN environment=$ROLLBAR_ENVIRONMENT revision=$git_revision
 echo deploy was completed successfully
